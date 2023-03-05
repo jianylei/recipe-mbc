6b9ba8cb04cf1c204c1049364438664a3d95904b
Jian yong Lei <jianyonglei@gmail.com>
2023-03-05 14:09:20 -0500

    Created useFetch hook to retrieve data

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,10 +1,14 @@
 import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import NotFound from "./NotFound"
+import useFetch from "../hooks/useFetch"
 
 const Recipe = () => {
     const { id } = useParams()
-    return (
-        <div>Recipe: {id}</div>
-    )
+
+    const { loading, error, data: recipe } = useFetch(`https://api.spoonacular.com/recipes/${id}/information?includeNutrition=false`)
+
+    return <div></div>
 }
 
 export default Recipe
---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react"
+
+/**
+ * @desc A custom hook to fetch data from an API
+ * @param {string} url - The URL to fetch
+ * @returns {object} { loading, error, data }  
+ */
+const useFetch = (url) => {
+    console.log(url)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        setLoading(true)
+        fetch(url, {
+            headers: new Headers({
+                'x-api-key': import.meta.env.VITE_SPOONACULAR_KEY
+            })
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            setData(data)
+        })
+        .catch((err) => {
+            console.error(err)
+            setError(err)
+        }).finally(() => {
+            setLoading(false)
+        })
+    }, [url])
+
+    return { loading, error, data }
+}
+
+export default useFetch
